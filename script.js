20897172059b80ed33acc7e3c09ffe5b2b37f3c0
5idneyD <sldriscoll@btinternet.com>
2021-12-30 23:53:51 +0000

    First Vue for loop

---
+++ script.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+	el: "#app",
+	data: {
+		newfeature: "",
+		features: ["You can create as many surveys as you want",
+			"There can be unlimited questions",
+			"As well as unlimited answers",
+			"And as many people can answer as you want",
+			"Only want certain people to answer?",
+		"Make it a Private Survey and give the Private Key to just them!"],
+	}
+});
