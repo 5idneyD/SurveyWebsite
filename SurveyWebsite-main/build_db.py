e52771ae92e9d40b4b01cb1588ef14f5e026b27b
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-01-06 14:12:37 +0000

    Add files via upload

---
+++ SurveyWebsite-main/build_db.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("accounts.db")
+c = conn.cursor()
+
+# c.execute("""
+# CREATE TABLE IF NOT EXISTS accounts (id int, username VARCHAR(255));""")
+
+# c.execute("INSERT INTO accounts values(1, 'Sidney');")
+
+for i in c.execute("SELECT * FROM sqlite_master;"):
+    print(i)
+conn.close()
---
+++ SurveyWebsite-main/static/js/create_survey.js
@@ -0,0 +1,7 @@
+var question_number = 1;
+$(document).ready(function(){
+	$("#addquestion").click(function(){
+	    $n = question_number.toString();
+		$("#questions").append("<ol class='new_questions'>Question" + question_number + "<input type='text' name='" + $n + "'>");
+		question_number += 1;});
+});
