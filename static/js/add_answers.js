c932ff539ea16a845f53fc772b1406114aa4ef7f
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-01-07 10:53:21 +0000

    Add files via upload

--- static/js/add_answers.js
+++ static/js/add_answers.js
@@ -1,21 +1,23 @@
 $num = 1;
 
-$(document).ready(function(){
-	$("button").click(function(){
+$(document).ready(function () {
+	$("button").click(function () {
 		var $question = $(this).attr("class");
 		console.log($question);
 		$cl = "#" + $question;
 		$($cl).append("<ol><input type='text' name='" + $question + $num.toString() + "'></ol>");
-		$num += 1;});
-	$("#submitbutton").click(function(){
+		$num += 1;
+	});
+	$("#submitbutton").click(function () {
 		$data = $("form").serialize()
 		$.ajax({
 			type: 'POST',
 			data: $("form").serialize(),
-			success: function(){
+			success: function (response) {
 				console.log($data);
+				window.location.href = response.redirect;
 			}
 
 		});
-		});
+	});
 });
