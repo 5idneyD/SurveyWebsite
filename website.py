0bf0f9e5ad315344aeb9a8aa26b0c6dec116f1f9
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-01-07 10:54:05 +0000

    Add files via upload

--- website.py
+++ website.py
@@ -1,42 +1,52 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__)) 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__,
             static_folder='./static',
             template_folder='./templates')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, "users.sqlite3")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + \
+    os.path.join(basedir, "users.sqlite3")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(12).hex()
 
 db = SQLAlchemy(app)
+
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     username = db.Column(db.String(100))
     password = db.Column(db.String(100))
     encrypted_username = db.Column(db.String(255))
 
+
 class Surveys(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     username = db.Column(db.String(100))
     survey_name = db.Column(db.String(255))
+
 
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     username = db.Column(db.String(100))
     survey_name = db.Column(db.String(255))
     question = db.Column(db.String(255))
-    
+
+
 class Answers(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    question = db.Column(db.String(255))
     answer = db.Column(db.String(255))
     score = db.Column(db.Integer)
-            
+
+
 db.create_all()
+
+
 @app.route("/")
 def index(methods=['POST', 'GET']):
     return render_template("index.html")
@@ -46,15 +56,15 @@ def index(methods=['POST', 'GET']):
 def signup():
     if request.method == "POST":
 
-            new_password = request.form['password']
-            new_username = request.form['username']
-            encrypted_name = ''.join(format(ord(x), "b") for x in new_username)
+        new_password = request.form['password']
+        new_username = request.form['username']
+        encrypted_name = ''.join(format(ord(x), "b") for x in new_username)
 
-                
-            new_user = Users(username=new_username, password=new_password, encrypted_username=encrypted_name)
-            db.session.add(new_user)
-            db.session.commit()
-        
+        new_user = Users(username=new_username, password=new_password,
+                         encrypted_username=encrypted_name)
+        db.session.add(new_user)
+        db.session.commit()
+
     return render_template("signup.html")
 
 
@@ -68,7 +78,7 @@ def login():
         current_username = request.form['username']
         current_password = request.form['password']
         print(current_username)
-        
+
         users = db.session.execute("SELECT * FROM users;")
         current_id = False
         for i in users:
@@ -78,89 +88,92 @@ def login():
                     print("Password does not match")
                     data = {
                         "message": "Password does not match"
-                        }
+                    }
                     return render_template("login.html", messages={'main': "Password does not match"})
                 else:
                     print("Logged in")
                     encrypted_username = i[3]
                     return redirect(f"/user/{encrypted_username}")
-                    
-                    
+
         if current_id == False:
             print("Username not found")
-            
+
     return render_template("login.html")
 
 
 @app.route("/user/<encrypted_username>", methods=['POST', 'GET'])
 def user_page(encrypted_username):
-    
-    user = db.session.execute("SELECT * FROM users WHERE encrypted_username='" + encrypted_username + "';")
+
+    user = db.session.execute(
+        "SELECT * FROM users WHERE encrypted_username='" + encrypted_username + "';")
     for i in user:
-        items=dict(i)
-    
+        items = dict(i)
+
     encrypted_name = items['encrypted_username']
     create_survey_link = "/user/" + encrypted_name + "/create_survey/"
 
-
     return render_template("userhome.html", user=user, items=items, create_survey_link=create_survey_link)
-
 
 
 @app.route("/user/<encrypted_username>/create_survey/", methods=['POST', 'GET'])
 def create_survey(encrypted_username):
 
-    if request.method=="POST":
+    if request.method == "POST":
 
         current_survey_name = request.form["surveyname"]
         data = dict(request.form)
         questions = []
-        
+
         for key, value in data.items():
             if key == "surveyname":
                 pass
             else:
                 questions.append(value)
         print(questions)
-        
-        user = db.session.execute("SELECT * FROM users WHERE encrypted_username='" + encrypted_username + "';")
+
+        user = db.session.execute(
+            "SELECT * FROM users WHERE encrypted_username='" + encrypted_username + "';")
         for i in user:
-            items=dict(i)
+            items = dict(i)
         survey_creator_username = items['username']
-        
-        new_survey = Surveys(username=survey_creator_username,  survey_name=current_survey_name)
+
+        new_survey = Surveys(username=survey_creator_username,
+                             survey_name=current_survey_name)
         db.session.add(new_survey)
-        
+
         for question in questions:
-            new_question = Questions(username=survey_creator_username,  survey_name=current_survey_name, question=question)
+            new_question = Questions(
+                username=survey_creator_username,  survey_name=current_survey_name, question=question)
             db.session.add(new_question)
-        
+
         db.session.commit()
-        
+
         return redirect(f"/user/{encrypted_username}/create_survey/{current_survey_name}/add_answers/")
-        
-        
+
     return render_template("create_survey.html")
 
 
 @app.route("/user/<encrypted_username>/create_survey/<current_survey_name>/add_answers/", methods=["POST", "GET"])
 def add_answer(encrypted_username, current_survey_name):
-    
-    questions = db.session.execute("SELECT question FROM questions WHERE survey_name='" + current_survey_name + "';")
 
-    print("---")
-    if request.method=="POST":
-        print("hello")
+    questions = db.session.execute(
+        "SELECT question FROM questions WHERE survey_name='" + current_survey_name + "';")
+
+    if request.method == "POST":
+
         data = dict(request.form)
-        print(data)
-        print("world")
+        for key, value in data.items():
+            key = key[:-1].replace("_", " ")
+            print(key, value)
+            new_answer = Answers(question=key, answer=value, score=0)
+            db.session.add(new_answer)
 
+        db.session.commit()
 
-            
-        
-
-    
+        # return redirect(f"/user/{encrypted_username}/", code=302)
+        return jsonify({'redirect': url_for("user_page", encrypted_username=encrypted_username)})
     return render_template("add_answers.html", questions=questions)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
