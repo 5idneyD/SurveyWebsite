5827bc22e40d08af535a8f1b5c37bff03148c6ce
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-01-19 08:52:28 +0000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-print(basedir)
