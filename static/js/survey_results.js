22ddee87dbf12176c22652b04984733cbd24e73f
Sidney <sldriscoll@btinternet.com>
2022-01-21 13:51:19 +0000

    Tidying up computer ui, mainly result charts

--- static/js/survey_results.js
+++ static/js/survey_results.js
@@ -14,15 +14,25 @@ var create_chart = function (question, labels, scores) {
             datasets: [{
                 label: question,
                 data: scores,
+                borderColor: ["rgba(255, 255, 255,1)"],
                 backgroundColor: [
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
-                
+
             }]
         },
+        options: {
+            scales: {
+                y: {
+                    suggestedMin: 0,
+                    suggestedMax: 8
+                }
+            }
+        }
     });
     var chart_id = document.getElementById("myChart");
     chart_id.setAttribute("id", "old_chart");
 }
+
